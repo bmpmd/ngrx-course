67136d0994c4b310c7a9147cf1e55b045e9990df
leo juarez <sk8leo@live.com>
2025-03-28 13:07:53 -0400

    added feature selector

--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,12 +1,18 @@
-import { createSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { AuthState } from "./reducers";
+
+export const selectAuthState = createFeatureSelector<AuthState>("auth")
 
 export const isLoggedIn = createSelector(
     //mapping function returns access to some slice of state in store
-    state=> state["auth"],
+    // state=> state["auth"],
+
+    //use feature selector as mapping, 
+    selectAuthState,
     // state => state["courses"]
     //final arg is the projector function, takes slice(s) of state
     //and return a value 
-    (authState) => !!authState.user 
+    (authState) => !!authState.user
 
 )
 
