cc7f2ec292e4368aa6160081811a92860242c0ec
leo juarez <sk8leo@live.com>
2025-03-31 14:47:40 -0400

    metareducer

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -46,4 +46,12 @@ export interface AppState{
 export  const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
-export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [] : [];
+
+export function logger(reducer: ActionReducer<any>): ActionReducer<any>{
+  return (state, action) => {
+    console.log("state before: " + state);
+    console.log("action ", action);
+    return reducer(state, action)
+  }
+}
+export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [logger] : [];
