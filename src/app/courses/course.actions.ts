3f761f72bc17c422c7249ed40456f33528b9e1f8
leo juarez <sk8leo@live.com>
2025-03-31 15:43:21 -0400

    effect for fetching courses

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -9,7 +9,7 @@ export const loadAllCourses = createAction(
 
 export const allCoursesLoaded = createAction(
     "[Load Courses Effect] All Courses Loaded",
-    props<{courses: Course[]}>
+    props<{courses: Course[]}>()
 );
 
 
